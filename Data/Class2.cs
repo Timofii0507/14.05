4183cd7af1f28cb6c791defe3186e2e443d6c180
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-07-11 10:55:27 +0300

    hm

--- Data/Class2.cs
+++ Data/Class2.cs
@@ -7,7 +7,7 @@ namespace DataAccess
     public class GameDbContext : DbContext
     {
         public DbSet<Game> Games { get; set; }
-
+         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=DESKTOP-GNKQNCJ\\SQLEXPRESS;Database=GameLibrary;Integrated Security=True;TrustServerCertificate=True;");
